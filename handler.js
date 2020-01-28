f76165edf2bf3ac9bc579e098bafbbee3954bac6
Harriet Ryder <harriethryder@gmail.com>
2020-01-28 16:15:41 +0000

    Connect to database for GET and PUT endpoints

--- handler.js
+++ handler.js
@@ -2,41 +2,34 @@ const express = require("express");
 const cors = require("cors");
 const serverlessHttp = require("serverless-http");
 const bodyParser = require("body-parser");
+const mysql = require("mysql");
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: "developers"
+});
 
 // GET /developers
 
 app.get("/developers", function(request, response) {
   // request is an object with lots of info about the request
   // response is an object which allows us to define what kind of response we want to send back
-
-  response.status(200).json({
-    developers: [
-      {
-        name: "Ena",
-        available: true,
-        date_joined: "2020-01-09",
-        skills: "HTML, CSS, React, NodeJS, AWS, Git",
-        id: 1
-      },
-      {
-        name: "Sue",
-        available: true,
-        date_joined: "2020-01-10",
-        skills: "Agile, NodeJS, React, C#",
-        id: 2
-      },
-      {
-        name: "Sitara",
-        available: false,
-        date_joined: "2020-01-11",
-        skills: "Java, NodeJS, Git, TDD",
-        id: 3
-      }
-    ]
+  connection.query("SELECT * FROM Developers", function(err, data) {
+    if (err) {
+      response.status(500).json({
+        error: err
+      });
+    } else {
+      response.status(200).json({
+        developers: data
+      });
+    }
   });
 });
 
@@ -48,13 +41,22 @@ app.put("/developers/:id", function(request, response) {
   // For sensitive data or larger pieces of data, I can use the request body
   { name: "Fred", available: true, skills: "HTML and CSS" }
   */
-
   const updatedDeveloper = request.body;
   const id = request.params.id;
 
-  response.status(200).json({
-    message: `Successfully updated developer ID ${id} with name: ${updatedDeveloper.name}, available: ${updatedDeveloper.available}, skills: ${updatedDeveloper.skills}`
-  });
+  // SQL injection
+  // Escape queries
+  connection.query(
+    `UPDATE Developers SET ? WHERE id=?`,
+    [updatedDeveloper, id],
+    function(err) {
+      if (err) {
+        response.status(500).json({ error: err });
+      } else {
+        response.sendStatus(200);
+      }
+    }
+  );
 });
 
 // DELETE /developers
